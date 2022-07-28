1312ff54bcc0ea6d83bcbea3701b7ca73e57fc94
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-28 10:46:22 -0300

    Update botoronga.py

--- botoronga.py
+++ botoronga.py
@@ -1,40 +1,100 @@
-#! /usr/bin/env python
-# coding=utf-8
-
 import tweepy
 import os
-import COVID19Py
-from covid import Covid
-import pandas
+import pandas as pd
+from datetime import date, timedelta, datetime
 from auth import api
 
-covid = Covid()
-br_cases = covid.get_status_by_country_id(24)
 
-confirmed = (br_cases.get('confirmed'))
-deaths = (br_cases.get('deaths'))
-active= (br_cases.get('active'))
+# Data de ontem
 
-df = pandas.read_csv('all_dead.csv')
-mortos = int(df.values[0][0])
-all_dead = deaths - mortos
-df.at[0,"Mortos"] = deaths
-df.to_csv('all_dead.csv', index=False)
+today = date.today()
+yesterday = today - timedelta(days=1)
+yesterday = str(yesterday)
 
-mystring = f"""COVID-19 no Brasil
+# Database do owid
+
+df = pd.read_csv(
+    'https://covid.ourworldindata.org/data/owid-covid-data.csv')
+
+loc = df['location']
+date = df['date']
+
+# Dados BR
+
+br_info = df[(loc == 'Brazil') & (date == yesterday)]
+
+confirmed = int(br_info['total_cases'])
+confirmed_24 = int(br_info['new_cases'])
+deaths = int(br_info['total_deaths'])
+deaths_24 = int(br_info['new_deaths'])
+
+mystring_br = f"""COVID-19 no Brasil
 
 Casos confirmados: {confirmed:,}
-Mortes: {deaths:,}
-Confirmados ativos: {active:,}"""
+Casos em 24h: {confirmed_24:,}
+Total de mortes: {deaths:,}
+Mortes em 24h: {deaths_24:,}
 
-#Fonte: https://ahmednafies.github.io/covid/john_hopkins/
+Fonte: https://covid.ourworldindata.org"""
 
-#Mortes em 24h: {all_dead:,}
+api.update_status(mystring_br)
 
-api.update_status(mystring)
+
+# Dados EUA
+
+us_info = df[(loc == 'United States') & (date == yesterday)]
+
+confirmed = int(us_info['total_cases'])
+confirmed_24 = int(us_info['new_cases'])
+deaths = int(us_info['total_deaths'])
+deaths_24 = int(us_info['new_deaths'])
+
+mystring_us = f"""COVID-19 nos EUA
+
+Casos confirmados: {confirmed:,}
+Casos em 24h: {confirmed_24:,}
+Total de mortes: {deaths:,}
+Mortes em 24h: {deaths_24:,}
+
+Fonte: https://covid.ourworldindata.org"""
+
+
+# Find the last tweet and reply
 
 toReply = api.me().screen_name
 
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+for tweet in tweets:
+        api.update_status("@" + toReply + mystring_us, in_reply_to_status_id = tweet.id)
+
+
+# Dados Mundo
+
+wrl_info = df[(loc == 'World') & (date == yesterday)]
+
+confirmed = int(wrl_info['total_cases'])
+confirmed_24 = int(wrl_info['new_cases'])
+deaths = int(wrl_info['total_deaths'])
+deaths_24 = int(wrl_info['new_deaths'])
+
+mystring_wrl = f"""COVID-19 no Mundo
+
+Casos confirmados: {confirmed:,}
+Casos em 24h: {confirmed_24:,}
+Total de mortes: {deaths:,}
+Mortes em 24h: {deaths_24:,}
+
+Fonte: https://covid.ourworldindata.org"""
+
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+for tweet in tweets:
+        api.update_status("@" + toReply + mystring_wrl, in_reply_to_status_id = tweet.id)
+
+
+
+#### Gráficos
 ### Gráfico de Vacina/centena
 
 tweets = api.user_timeline(screen_name = toReply, count=1)
@@ -51,16 +111,11 @@ for tweet in tweets:
 
 ### Gráfico de porcentagem
 
-df = pandas.read_csv('all_dead.csv')
-percent = float(df.values[1][0])
-vacs = int(df.values[2][0])
+mystring_vacbr = f""" Vacinações por Milhão pelo Mundo
 
-mystring_vacbr = f""" Total de Brasileiros Vacinados: {vacs:,}
+Dados relacionados à pessoas vacinadas com pelo menos uma dose.
 
-Porcentagem de Brasileiros Vacinados: {percent}%
-
-
-Dados relacionados à pessoas vacinadas com pelo menos uma dose."""
+Fonte: https://covid.ourworldindata.org/"""
 
 tweets = api.user_timeline(screen_name = toReply, count=1)
 
@@ -68,70 +123,89 @@ for tweet in tweets:
         api.update_with_media('covid_vactot.png', "@" + toReply + mystring_vacbr, in_reply_to_status_id = tweet.id)
 
 
-### Gráfico de Casos India
+### Gráfico de Casos Mundo
 
 tweets = api.user_timeline(screen_name = toReply, count=1)
 
-mystring_vac = """ Novos Casos Diários Confirmados no Brasil e na Índia
-"""
+mystring_vac = """ Novos Casos Diários Confirmados no Mundo
+
+Fonte: https://covid.ourworldindata.org"""
+
+for tweet in tweets:
+        api.update_with_media('world_cases.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
+
+
+### Gráfico de Casos BR
+
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+mystring_vac = """ Novos Casos Diários Confirmados no Brasil
+
+Fonte: https://covid.ourworldindata.org"""
+
+for tweet in tweets:
+        api.update_with_media('br_cases.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
+
+
+
+### Gráfico de Casos EUA, BR, FR
+
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+mystring_vac = """ Novos Casos Diários Confirmados no Brasil, EUA e França
+
+Fonte: https://covid.ourworldindata.org"""
 
 for tweet in tweets:
         api.update_with_media('covid_ind_cases.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
 
 
-### Gráfico de Mortes 24h India
+
+### Gráfico de Mortes 24h Mundo
 
 tweets = api.user_timeline(screen_name = toReply, count=1)
 
-mystring_vac = """ Mortes em 24h no Brasil e na Índia
-"""
+mystring_vac = """ Mortes em 24h no Mundo
+
+Fonte: https://covid.ourworldindata.org"""
+
+for tweet in tweets:
+        api.update_with_media('world_deaths.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
+
+
+### Gráfico de Mortes 24h Brasil
+
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+mystring_vac = """ Mortes em 24h no Brasil
+
+Fonte: https://covid.ourworldindata.org"""
+
+for tweet in tweets:
+        api.update_with_media('br_deaths.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
+
+
+
+### Gráfico de Mortes 24h Brasil, EUA e França
+
+tweets = api.user_timeline(screen_name = toReply, count=1)
+
+mystring_vac = """ Mortes em 24h no Brasil, EUA e França
+
+Fonte: https://covid.ourworldindata.org"""
 
 for tweet in tweets:
         api.update_with_media('covid_ind_deaths.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
 
 
-### Gráfico de Mortes Total India
+### Gráfico de Mortes Total EUA
 
 
 tweets = api.user_timeline(screen_name = toReply, count=1)
 
-mystring_vac = """ Total de Mortes no Brasil e na Índia
-"""
+mystring_vac = """ Total de Mortes no Brasil e nos EUA
+
+Fonte: https://covid.ourworldindata.org"""
 
 for tweet in tweets:
         api.update_with_media('covid_ind_totdeaths.png', "@" + toReply + mystring_vac, in_reply_to_status_id = tweet.id)
-
-
-try:
-    covid19 = COVID19Py.COVID19(data_source="jhu")
-
-    latest = covid19.getLatest()
-
-    location_us = covid19.getLocationByCountryCode("US")
-    items_us = location_us[0].get('latest')
-
-    confirmed_all = (latest.get('confirmed'))
-    deaths_all = (latest.get('deaths'))
-
-    confirmed_us = (items_us.get('confirmed'))
-    deaths_us = (items_us.get('deaths'))
-
-    mystring_all = f""" COVID-19 no mundo
-
-Casos confirmados: {confirmed_all:,}
-Mortes: {deaths_all:,}
-
-COVID-19 nos EUA
-
-Casos confirmados: {confirmed_us:,}
-Mortes: {deaths_us:,}
-
-Fonte: https://systems.jhu.edu/research/public-health/ncov/"""
-
-    tweets = api.user_timeline(screen_name = toReply, count=1)
-
-    for tweet in tweets:
-        api.update_status("@" + toReply + mystring_all, in_reply_to_status_id = tweet.id)
-
-except:
-    pass
