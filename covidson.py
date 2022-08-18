8f47a9a1dd8d5a07790916ceec9e1f83782395c9
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-18 10:32:51 -0300

    Update covidson.py

--- covidson.py
+++ covidson.py
@@ -81,7 +81,7 @@ plt.xlabel('Data', fontsize=12, alpha=0.9)
 plt.title('Mortes em 24h por COVID-19 no mundo', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("world_deaths")
+plt.savefig("data/world_deaths")
 
 
 
@@ -158,7 +158,7 @@ plt.xlabel('Data', fontsize=12, alpha=0.9)
 plt.title('Novos casos mundiais COVID-19', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("world_cases")
+plt.savefig("data/world_cases")
 
 
 # Step 1: Load the data
@@ -234,7 +234,7 @@ plt.xlabel('Data', fontsize=12, alpha=0.9)
 plt.title('Novos casos COVID-19 no Brasil', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("br_cases")
+plt.savefig("data/br_cases")
 
 
 
@@ -311,7 +311,7 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Vacinações COVID-19', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("covid_vac")
+plt.savefig("data/covid_vac")
 
 
 # Step 1: Load the data
@@ -415,7 +415,7 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Vacinações COVID-19', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("covid_vactot")
+plt.savefig("data/covid_vactot")
 
 def percentage(value, total, multiply=True):
 	try:
@@ -530,7 +530,7 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Novos casos de COVID-19 no Brasil, EUA e França', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("covid_ind_cases")
+plt.savefig("data/covid_ind_cases")
 
 #Gráfico de Novas Mortes
 
@@ -609,7 +609,7 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Mortes em 24h por COVID-19 no Brasil, EUA e França', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("covid_ind_deaths")
+plt.savefig("data/covid_ind_deaths")
 
 #Gráfico de mortes total
 
@@ -688,7 +688,7 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Total de Mortes por COVID-19 no Brasil e nos EUA', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("covid_ind_totdeaths")
+plt.savefig("data/covid_ind_totdeaths")
 
 
 # Step 1: Load the data
@@ -764,4 +764,84 @@ plt.xlabel('Date', fontsize=12, alpha=0.9)
 plt.title('Mortes em 24h por COVID-19 no Brasil', fontsize=18, weight='bold', alpha=0.9)
 
 # D) Celebrate!
-plt.savefig("br_deaths")
+plt.savefig("data/br_deaths")
+
+
+#Gráfico de mortes total no mundo
+
+# Step 1: Load the data
+df = pd.read_csv(
+    'https://covid.ourworldindata.org/data/owid-covid-data.csv',
+    usecols=['date', 'location', 'total_deaths'],
+    parse_dates=['date'])
+
+countries = ['World']
+df = df[df['location'].isin(countries)]
+
+
+
+# Step 2: Summarize the data
+pivot = pd.pivot_table(
+    data=df,                                    # What dataframe to use
+    index='date',                               # The "rows" of your dataframe
+    columns='location',                         # What values to show as columns
+    values='total_deaths',    # What values to aggregate
+    aggfunc='mean',                             # How to aggregate data
+    )
+
+pivot = pivot.fillna(method='ffill')
+
+
+# Step 3: Set up key variables for the visualization
+main_country = 'Brazil'
+colors = {country:('black' if country!= main_country else '#129583') for country in countries}
+alphas = {country:(0.5 if country!= main_country else 1.0) for country in countries}
+
+
+# Step 4: Plot all countries
+fig, ax = plt.subplots(figsize=(12,8))
+fig.patch.set_facecolor('#F5F5F5')    # Change background color to a light grey
+ax.patch.set_facecolor('#F5F5F5')     # Change background color to a light grey
+
+for country in countries:
+    ax.plot(
+        pivot.index,              # What to use as your x-values
+        pivot[country],           # What to use as your y-values
+        color=colors[country],    # How to color your line
+        alpha=alphas[country]     # What transparency to use for your line
+    )
+    ax.text(
+        x = pivot.index[-1] + timedelta(days=2),    # Where to position your text relative to the x-axis
+        y = pivot[country].max(),                   # How high to position your text
+        color = colors[country],
+        fontsize=13,                    # What color to give your text
+        s = country,                                # What to write
+        alpha=alphas[country]                       # What transparency to use
+    )
+    #vacs_br = pivot['new_deaths_smoothed']
+
+
+
+# Step 5: Configures axes
+## A) Format what shows up on axes and how it's displayed
+date_form = DateFormatter("%d-%m-%Y")
+ax.xaxis.set_major_locator(WeekdayLocator(byweekday=(0), interval=3))
+ax.xaxis.set_major_formatter(date_form)
+plt.xticks(rotation=45, fontsize=12)
+
+
+## B) Customizing axes and adding a grid
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color('#3f3f3f')
+ax.spines['left'].set_color('#3f3f3f')
+ax.tick_params(colors='#3f3f3f')
+ax.grid(alpha=1)
+
+## C) Adding a title and axis labels
+plt.ylabel('Total de Mortes', fontsize=18, alpha=0.9)
+plt.xlabel('Date', fontsize=12, alpha=0.9)
+plt.title('Total de Mortes por COVID-19 no Mundo', fontsize=18, weight='bold', alpha=0.9)
+
+# D) Celebrate!
+plt.savefig("data/world_totdeaths")
